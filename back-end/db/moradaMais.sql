d1638c95eec9779fba33c0a698fb2a3374362639
roxreis <rodrigoassuncao12@gmail.com>
2021-01-13 17:34:04 -0300

    incluido arquivo com bd

--- back-end/db/moradaMais.sql
+++ back-end/db/moradaMais.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 06/11/2020 às 22:55
+-- Tempo de geração: 13/01/2021 às 21:32
 -- Versão do servidor: 10.4.13-MariaDB
 -- Versão do PHP: 7.4.8
 
@@ -39,6 +39,34 @@ CREATE TABLE `admin` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `locador`
+--
+
+CREATE TABLE `locador` (
+  `id_imovel` int(11) NOT NULL,
+  `descricao_imovel` varchar(500) NOT NULL,
+  `regiao` varchar(100) NOT NULL,
+  `img` varchar(250) DEFAULT NULL,
+  `img2` varchar(250) DEFAULT NULL,
+  `img3` varchar(250) DEFAULT NULL,
+  `id_user` int(11) NOT NULL,
+  `data_cadast` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Despejando dados para a tabela `locador`
+--
+
+INSERT INTO `locador` (`id_imovel`, `descricao_imovel`, `regiao`, `img`, `img2`, `img3`, `id_user`, `data_cadast`) VALUES
+(1, '2 quartos', 'Zona Leste', NULL, NULL, NULL, 3, '2021-01-13'),
+(2, '3 eeeee', 'Zona Oeste', NULL, NULL, NULL, 1, '2021-01-13'),
+(3, '2 quartos', 'Zona Leste', NULL, NULL, NULL, 3, '2021-01-13'),
+(4, '3 eeeee', 'Zona Oeste', NULL, NULL, NULL, 1, '2021-01-13'),
+(5, '2 quartos', 'Zona Leste', '', '', '', 5, '2021-01-13');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `mensagem`
 --
 
@@ -60,33 +88,32 @@ INSERT INTO `mensagem` (`id_msg`, `nome_msg`, `email_msg`, `tel_msg`, `texto_msg
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `usuarios`
+-- Estrutura para tabela `usuario`
 --
 
-CREATE TABLE `usuarios` (
-  `user_id` int(11) UNSIGNED NOT NULL,
+CREATE TABLE `usuario` (
+  `id_user` int(11) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `user_pass` varchar(100) NOT NULL,
   `lembrete_senha` varchar(200) NOT NULL,
   `user_cpf` varchar(11) NOT NULL,
   `user_email` varchar(100) NOT NULL,
-  `user_nivel` tinyint(1) UNSIGNED NOT NULL DEFAULT 1,
+  `user_nivel` tinyint(1) NOT NULL DEFAULT 1,
   `user_ativo` tinyint(1) NOT NULL DEFAULT 1,
-  `check_locador` varchar(1) DEFAULT NULL,
-  `check_locatario` varchar(1) DEFAULT NULL,
-  `regiao` varchar(50) DEFAULT NULL,
-  `descricao_imovel` varchar(500) DEFAULT NULL,
-  `cadastro` datetime DEFAULT current_timestamp()
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+  `bio` varchar(500) NOT NULL,
+  `cadastro` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `usuarios`
+-- Despejando dados para a tabela `usuario`
 --
 
-INSERT INTO `usuarios` (`user_id`, `first_name`, `last_name`, `user_pass`, `lembrete_senha`, `user_cpf`, `user_email`, `user_nivel`, `user_ativo`, `check_locador`, `check_locatario`, `regiao`, `descricao_imovel`, `cadastro`) VALUES
-(64, 'teste', 'Teste Sobrenome', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'teste', '11120119863', 'teste@teste', 1, 1, '1', '', 'Escolha uma opção', '', '2020-11-06 00:30:35'),
-(63, 'Elsa', 'Soares', '1c6637a8f2e1f75e06ff9984894d6bd16a3a36a9', 'toto', '99999999999', 'elsa@soares', 1, 1, '', '', 'Escolha uma opção', '\"\"\"\"', '2020-11-05 23:42:58');
+INSERT INTO `usuario` (`id_user`, `first_name`, `last_name`, `user_pass`, `lembrete_senha`, `user_cpf`, `user_email`, `user_nivel`, `user_ativo`, `bio`, `cadastro`) VALUES
+(1, 'rodrigo', 'reis', '123', '123', '12345678945', 'rodrigo@rodrigo', 1, 1, 'sou legal', '2021-01-13'),
+(3, 'rodrigo', 'reis', '123', '123', '12345678945', 'rodrigo@rodrigo', 1, 1, 'sou legal', '2021-01-13'),
+(5, 'Rodrigo Aparecido de Assunção', 'Reis', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'eee', '99999999999', 'rodrigoassuncao12@gmail.com', 1, 1, 'eeee', '2021-01-13'),
+(6, 'Elsa', 'Reis', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'ee', '11120119863', 'elsa@soares', 1, 1, 'ee', '2021-01-13');
 
 --
 -- Índices de tabelas apagadas
@@ -100,19 +127,23 @@ ALTER TABLE `admin`
   ADD UNIQUE KEY `admin_email` (`admin_email`);
 
 --
+-- Índices de tabela `locador`
+--
+ALTER TABLE `locador`
+  ADD PRIMARY KEY (`id_imovel`),
+  ADD KEY `id_user` (`id_user`);
+
+--
 -- Índices de tabela `mensagem`
 --
 ALTER TABLE `mensagem`
   ADD PRIMARY KEY (`id_msg`);
 
 --
--- Índices de tabela `usuarios`
+-- Índices de tabela `usuario`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `user_email` (`user_email`),
-  ADD UNIQUE KEY `user_cpf` (`user_cpf`),
-  ADD KEY `user_nivel` (`user_nivel`);
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT de tabelas apagadas
@@ -125,16 +156,32 @@ ALTER TABLE `admin`
   MODIFY `id_admin` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
+-- AUTO_INCREMENT de tabela `locador`
+--
+ALTER TABLE `locador`
+  MODIFY `id_imovel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT de tabela `mensagem`
 --
 ALTER TABLE `mensagem`
   MODIFY `id_msg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de tabela `usuarios`
+-- AUTO_INCREMENT de tabela `usuario`
 --
-ALTER TABLE `usuarios`
-  MODIFY `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+ALTER TABLE `usuario`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Restrições para dumps de tabelas
+--
+
+--
+-- Restrições para tabelas `locador`
+--
+ALTER TABLE `locador`
+  ADD CONSTRAINT `user_id_fk` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
